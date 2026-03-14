b4e3b6f89d9d8e06a6c56e52f70e7e25de389c45
shabnusince1988-tech <shabnusince1988@gmail.com>
2026-03-14 13:15:31 +0530

    Delete Services directory

--- Services/Interfaces/IGeofenceService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace AmbulanceAPI.Services.Interfaces
-{
-    public interface IGeofenceService
-    {
-        bool IsWithinRange(decimal lat1, decimal lng1, decimal lat2, decimal lng2, double rangeInMeters);
-    }
-}
